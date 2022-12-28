51815e6e85729f17ef08e53dd763468482bd34d5
Javier Cervera <jvrmgmt@gmail.com>
2022-12-28 16:49:41 -0500

    form names

--- pages/index.js
+++ pages/index.js
@@ -398,7 +398,31 @@ export default function Home() {
             </form>
           </div>
         </section>
+        <section></section>
       </main>
+
+      <footer class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-900">
+        <div class="sm:flex sm:items-center sm:justify-between">
+          <a href="/" class="flex items-center mb-4 sm:mb-0">
+            <div className="rounded-sm  bg-stone-900">
+              <h1 className="text-3xl text-stone-100 dark:text-stone-100 font-good ">
+                J<span className="text-red-600 hvr-grow">V</span>R
+              </h1>
+            </div>
+            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+              Flowbite
+            </span>
+          </a>
+        </div>
+        <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2022{' '}
+          <a href="/" class="hover:underline">
+            JVR™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </footer>
     </div>
   );
 }
