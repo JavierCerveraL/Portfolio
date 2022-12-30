ffd41cf6ceb8f44f099e8e3563737fae54137b0a
Javier Cervera <jvrmgmt@gmail.com>
2022-12-29 21:08:24 -0500

    resume

--- pages/index.js
+++ pages/index.js
@@ -66,6 +66,9 @@ export default function Home() {
               </li>
               <li>
                 <a
+                  href="https://drive.google.com/file/d/1SwyarMF6XPTW_2I9BYy-wye2dkCt2vl9/view?usp=share_link"
+                  target="_blank"
+                  rel="noopener noreferrer"
                   className=" bg-gradient-to-r from-red-600
                 to-rose-800 text-white px-4 py-2 rounded-md ml-8"
                 >
